200c203536914625dac4580f8f4adbb89ae188bc
Paul Pirie <pauljohnpirie@yahoo.co.uk>
2020-10-11 02:25:35 +0100

    Debug X

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_socketio import SocketIO, send, emit
-import eventlet
 
 # eventlet.monkey_patch()
 
